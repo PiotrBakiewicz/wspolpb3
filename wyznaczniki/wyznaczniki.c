ded87e289acc40d8794b20ef0aea238ff3113faa
PiotrBakiewicz <mojave@onet.eu>
2017-11-23 09:43:46 +0100

    wyznaczniki

---
+++ wyznaczniki/wyznaczniki.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+void wczytaj(double &a1, double &b1, double &c1, double &a2, double &b2, double &c2){ 
+
+
+puts("Podaj wspolczynnik a1); scanf("%lf", &a1);
+puts("Podaj wspolczynnik b1); scanf("%lf", &b1);
+puts("Podaj wspolczynnik c1); scanf("%lf", &c1);
+puts("Podaj wspolczynnik a2); scanf("%lf", &a2);
+puts("Podaj wspolczynnik b2); scanf("%lf", &b2);
+puts("Podaj wspolczynnik c2); scanf("%lf", &c2);
+ 
+}
+int main (){
+	double a1, b1, c1, a2, b2, c2;
